4d7526b74b6effb0d7db01be791aa061f33d84dd
Frans Slothouber <frans.slothouber@gmail.com>
2014-06-13 11:15:20 +0200

    rf code cleanup

--- RandomForest/rf.r
+++ RandomForest/rf.r
@@ -4,39 +4,45 @@ require(randomForest)
 
 load(file="../Processed/norm_dataset.rdata")
 
-ll <- dataset$Label
 # Give NAs a value. Except for the Label NA,
 # otherwise randomForest will complain in the predict phase.
+ll <- dataset$Label
 dataset$Label <- NULL
 dataset[is.na(dataset)] <- -999
 dataset$Label <- ll
 
-feature_subset <- grep("PRI|DER",colnames(dataset),value=TRUE)
+feature_subset <- grep("PRI|DER", colnames(dataset), value=TRUE)
+training <- dataset[dataset$test == FALSE, c(feature_subset, "Label")]
 
-training <- dataset[dataset$test == FALSE, c(feature_subset,"Label")]
+# Train
+model <- randomForest(Label~., training, ntree=50, do.trace=TRUE)
 
-model <- randomForest( Label~., training, ntree=50, do.trace=TRUE)
 
-signal_percentage <- function(no_s, no_b) {
-    100.0*(no_s/(no_s+no_b))
-}
-
+# Predict
 testset <- dataset[dataset$test == TRUE, feature_subset]
 yp <- predict(model, testset, type="prob")
 
 kaggle_prediction <- data.frame( 
     EventID=dataset[dataset$test == TRUE, "EventId"],
     RankOrder=0,
-    Class=ifelse(yp[,'s']>0.5,'s','b')
+    Class=ifelse(yp[,'s']>0.5, 's', 'b')
 )
 
+# Sort according to 's' probability so we can have a RankOrder.
 s_prob <- yp[,'s']
 sorted_prob <- sort(s_prob, decreasing=FALSE, index.return=TRUE)
 
 kaggle_prediction <- kaggle_prediction[sorted_prob$ix,]
 kaggle_prediction$RankOrder <- 1:nrow(kaggle_prediction)
 
-write.table(kaggle_prediction, file="full_prediction.csv", 
+signal_percentage <- function(no_s, no_b) {
+    100.0*(no_s/(no_s+no_b))
+}
+
+
+write.table(kaggle_prediction, file="full_prediction.csv",
     sep=",", row.names=FALSE, quote=FALSE)
 
 # Scored     2.87001
+# TODO Compute AMS
+#
