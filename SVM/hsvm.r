de987b3f157316c3ed9a4d83f57e5d65d55822c1
Frans Slothouber <frans.slothouber@gmail.com>
2014-06-18 09:27:12 +0200

    SVM prototype

---
+++ SVM/hsvm.r
@@ -0,0 +1,19 @@
+set.seed(76514345)
+rm(list=ls())
+require(e1071)
+
+load(file="../Processed/norm_dataset.rdata")
+
+response_label <- dataset[dataset$test==FALSE, "Label"]
+dataset$Label  <- NULL
+
+# Give NAs a value.
+dataset[is.na(dataset)] <- 50
+
+feature_subset <- grep("PRI_jet_num|DER", colnames(dataset), value=TRUE)
+training <- dataset[dataset$test == FALSE, feature_subset]
+
+# Train
+model <- svm(x=training, y=response_label)
+
+
