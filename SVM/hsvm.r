148637c125afccc1e00fddbffb21e73c6c3ed7af
Frans Slothouber <frans.slothouber@gmail.com>
2014-06-20 08:03:34 +0200

    SVM on a subset

--- SVM/hsvm.r
+++ SVM/hsvm.r
@@ -1,4 +1,5 @@
 set.seed(76514345)
+
 rm(list=ls())
 require(e1071)
 
@@ -12,6 +13,10 @@ dataset[is.na(dataset)] <- 50
 
 feature_subset <- grep("PRI_jet_num|DER", colnames(dataset), value=TRUE)
 training <- dataset[dataset$test == FALSE, feature_subset]
+idx <- sample.int(nrow(dataset),2000)
+training <- training[idx, ]
+response_label <- response_label[idx]
+
 
 # Train
 model <- svm(x=training, y=response_label)
